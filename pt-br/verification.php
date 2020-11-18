73eb38a4ded718dc00a421d4ccf0082afd45353c
William Mandai <wm@gitbench.com>
2020-11-18 23:49:23 +0300

    Updated language files v3.0

--- pt-br/verification.php
+++ pt-br/verification.php
@@ -1,11 +1,11 @@
 <?php
 return [
-	'verification_email_subject' => 'Verificação de Conta',
+    'verification_email_subject' => 'Verificação de Conta',
 
-	// error view
-	'verification_error_header' => 'Falha na verificação',
-	'verification_error_message' => 'Sua conta não pôde ser verificada.',
-	'verification_error_back_button' => 'Voltar',
-	'resend' => 'Reenviar'
+    // error view
+    'verification_error_header' => 'Falha na verificação',
+    'verification_error_message' => 'Sua conta não pôde ser verificada.',
+    'verification_error_back_button' => 'Voltar',
+    'resend' => 'Reenviar'
 
 ];
